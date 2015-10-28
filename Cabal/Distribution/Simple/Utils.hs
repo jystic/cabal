c3c48445763fe688cc7b0020b656d307a709fa27
Jacob Stanley <jacob@stanley.io>
2015-10-29 08:48:38 +1100

    Changed package name to Cabal-hdevtools
    We can get hdevtools to depend on Cabal-1.22 while still supporting GHC
    7.8.4 if we just change the name of the package.
    
    The isn't normally possible because the ghc package depends on
    Cabal-1.18.

--- Cabal/Distribution/Simple/Utils.hs
+++ Cabal/Distribution/Simple/Utils.hs
@@ -199,13 +199,13 @@ import Distribution.Compat.Exception
 import Distribution.Verbosity
 
 #ifdef VERSION_base
-import qualified Paths_Cabal (version)
+import qualified Paths_Cabal_hdevtools (version)
 #endif
 
 -- We only get our own version number when we're building with ourselves
 cabalVersion :: Version
 #if defined(VERSION_base)
-cabalVersion = Paths_Cabal.version
+cabalVersion = Paths_Cabal_hdevtools.version
 #elif defined(CABAL_VERSION)
 cabalVersion = Version [CABAL_VERSION] []
 #else
